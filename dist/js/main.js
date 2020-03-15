beac114db9161959c726cac503b6f66f1b08de28
unknown <simfolkerts@gmail.com>
2020-03-15 21:48:42 +1300

    front page basically finished with toggle menu

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  const El_burgerBtn = document.querySelector('.menu-btn');
+  const El_nav = document.querySelector('.nav');
+  const hideModifier = 'nav--hidden';
+  
+  El_burgerBtn.addEventListener('click', function() {
+    El_nav.classList.toggle(hideModifier);
+  });
+}
